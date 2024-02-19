1832330c895ca48b6e3436b6301069791f86563a
remigerme <remi.germe@gmail.com>
2024-02-19 16:45:16 +0100

    sample z in header

--- sampling.h
+++ sampling.h
@@ -7,5 +7,11 @@ void TrapSamp(poly_matrix Bx, poly_matrix T, poly_matrix Tx);
 // Assigns uniformely a random polynomial in Rq
 void Sample_Rq_uniform(poly a);
 
-// Assigns a polynomial according to discrete gaussian distribution D(R, sigma)
-void Sample_D_centered(signed_poly a, real sigma);
+// Samples from D_{R,sigma} where R = Z[X] / <X^n+1> is isomorphic to Z^n
+void Sample_R_centered(signed_poly a, real sigma);
+
+/*
+Samples from distribution D_{c,sigma}, ie
+Samples an element in Z with probability proportionnal to e^{-(c-x)^2/2*(sigma^2)}
+*/
+signed_scalar sample_Z(real c, real sigma);
