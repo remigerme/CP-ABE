2bdfacc5fe876345e201be53830be52655694b7d
remigerme <remi.germe@gmail.com>
2024-02-19 18:07:25 +0100

    moving dgs utils functions

--- random.h
+++ random.h
@@ -1,13 +1,8 @@
 #include "common.h"
 
-/*
-Returns an integer sampled from the uniform distribution over [0, q-1]
-using the uniform distribution over [0, 2^32 - 1] provided by random_bytes
-*/
+// Returns an integer sampled from the uniform distribution over [0, q-1]
 scalar uniform_mod_q(void);
 
-/*
-Samples from distribution D_{mu,sigma}, ie
-Samples an element in Z with probability proportionnal to e^{-(mu-x)^2/2*(sigma^2)}
-*/
-signed_scalar algorithmF(const real mu, const real sigma);
+// Discrete gaussian distribution utils functions
+dgs_disc_gauss_dp_t *init_dgs();
+void clear_dgs(dgs_disc_gauss_dp_t *D);
--- sampling.c
+++ sampling.c
@@ -1,27 +1,14 @@
 #include "sampling.h"
+#include "random.h"
 #include "dgs/dgs.h"
 
-/*
-Discrete gaussian distribution utils functions
-*/
-
-dgs_disc_gauss_dp_t *init_dgs()
+void Sample_Rq_uniform(poly a)
 {
-    // Cutoff `τ`, samples outside the range `(⌊c⌉-⌈στ⌉,...,⌊c⌉+⌈στ⌉)` are
-    // considered to have probability zero. This bound applies to algorithms
-    // which sample from the uniform distribution.
-    dgs_disc_gauss_dp_t *D = dgs_disc_gauss_dp_init(PARAM_SIGMA, 0, PARAM_TAU, DGS_DISC_GAUSS_UNIFORM_TABLE);
-    return D;
-}
-
-void clear_dgs(dgs_disc_gauss_dp_t *D)
-{
-    dgs_disc_gauss_dp_clear(D);
-}
-
-/*
-Discrete gaussian distribution sampling
-*/
+    for (int i = 0; i < PARAM_N; i++)
+    {
+        a[i] = uniform_mod_q();
+    }
+};
 
 void Sample_R_centered(signed_poly a, dgs_disc_gauss_dp_t *D)
 {
