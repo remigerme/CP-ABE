76498fd5caf44bae083075a2d4690e17e19f3e16
remigerme <remi.germe@gmail.com>
2024-02-19 16:49:53 +0100

    dummy params and access poly matrix element

--- common.h
+++ common.h
@@ -1,6 +1,10 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
+// Dummy parameters
+#define PARAM_N 10
+#define PARAM_Q 20
+
 typedef uint32_t scalar;
 typedef int32_t signed_scalar;
 typedef double real;
@@ -12,3 +16,6 @@ typedef signed_scalar *signed_poly;
 // Matrix of polynomials (single pointer on scalars, everything is accessed through macros)
 typedef scalar *poly_matrix;
 typedef signed_scalar *signed_poly_matrix;
+
+// Accessing polynomial Mi,j
+#define poly_matrix_element(M, nb_col, i, j) (&M[(PARAM_N) * (((i) * (nb_col)) + (j))])
