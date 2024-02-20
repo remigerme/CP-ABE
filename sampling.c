7005c32e04b20fb6ed87cd9a681d8e5eb911764f
remigerme <remi.germe@gmail.com>
2024-02-20 23:14:20 +0100

    using sampler struct

--- sampling.c
+++ sampling.c
@@ -1,16 +1,22 @@
 #include "sampling.h"
 
-#include "lib/dgs/dgs.h"
 #include "random.h"
 
-void Sample_Rq_uniform(poly a, CSPRNG rng) {
+sampler create_sampler() {
+    CSPRNG rng = create_csprng();
+    dgs_disc_gauss_dp_t *D = create_dgs();
+    sampler s = {rng, D};
+    return s;
+}
+
+void Sample_Rq_uniform(poly a, sampler s) {
     for (int i = 0; i < PARAM_N; i++) {
-        a[i] = uniform_mod_q(rng);
+        a[i] = uniform_mod_q(s.rng);
     }
 }
 
-void Sample_R_centered(signed_poly a, dgs_disc_gauss_dp_t *D) {
+void Sample_R_centered(signed_poly a, sampler s) {
     for (int i = 0; i < PARAM_N; ++i) {
-        a[i] = (signed_scalar)(D->call(D));
+        a[i] = (signed_scalar)(s.D->call(s.D));
     }
 }
--- sampling.h
+++ sampling.h
@@ -4,8 +4,19 @@ void TrapGen(poly_matrix B, poly_matrix T);
 
 void TrapSamp(poly_matrix Bx, poly_matrix T, poly_matrix Tx);
 
+typedef struct _sampler {
+    CSPRNG rng;              // for uniform distribution
+    dgs_disc_gauss_dp_t *D;  // for gaussian discrete distribution
+} sampler;
+
+// Create a sampler, used by sampling functions to generate random results
+sampler create_sampler(void);
+
 // Assigns uniformely a random polynomial in Rq
-void Sample_Rq_uniform(poly a, CSPRNG rng);
+void Sample_Rq_uniform(poly a, sampler s);
+
+// Assigns uniformely polynomials in Rq over a random matrix of size m * n
+void Sample_Rq_uniform_matrix(poly_matrix A, size_t m, size_t n, CSPRNG rng);
 
 // Samples from D_{R,sigma} where R = Z[X] / <X^n+1> is isomorphic to Z^n
-void Sample_R_centered(signed_poly a, dgs_disc_gauss_dp_t *D);
+void Sample_R_centered(signed_poly a, sampler s);
