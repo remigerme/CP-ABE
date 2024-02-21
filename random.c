35c52bc8b6b2aebd8e6ae14656c1b6e65ff739cb
remigerme <remi.germe@gmail.com>
2024-02-21 16:27:38 +0100

    new dgs algo (faster, same results)

--- random.c
+++ random.c
@@ -51,9 +51,8 @@ dgs_disc_gauss_dp_t* create_dgs() {
     // Cutoff `τ`, samples outside the range `(⌊c⌉-⌈στ⌉,...,⌊c⌉+⌈στ⌉)` are
     // considered to have probability zero. This bound applies to algorithms
     // which sample from the uniform distribution.
-    dgs_disc_gauss_dp_t* D = dgs_disc_gauss_dp_init(
-        PARAM_SIGMA, 0, PARAM_TAU, DGS_DISC_GAUSS_UNIFORM_TABLE);
-    return D;
+    return dgs_disc_gauss_dp_init(PARAM_SIGMA, 0, PARAM_TAU,
+                                  DGS_DISC_GAUSS_ALIAS);
 }
 
 signed_scalar signed_scalar_dgs(dgs_disc_gauss_dp_t* D) { return D->call(D); }
