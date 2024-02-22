6558cc88e520aabcbda7722a6ddcff6771a9b29b
remigerme <remi.germe@gmail.com>
2024-02-22 21:55:28 +0100

    circuit basic header (stored as btree)

---
+++ circuit.h
@@ -0,0 +1,23 @@
+#include "common.h"
+
+/*
+We store a circuit as a binary tree which indicates us
+in which order we should compute the NAND gates.
+The field `n` is relevant only if we're located
+at a leaf ie left == right == NULL, in which case
+it stores the relevant index `n`.
+*/
+struct btree {
+    struct btree *left;
+    struct btree *right;
+    int n;
+};
+
+typedef struct btree circuit;
+
+/*
+Given A = [A1 | ... | Ak] we compute
+Af = f(A1, ..., Ak) where f is a boolean circuit
+composed of NAND gates.
+*/
+void compute_Af(poly_matrix A, circuit f, poly_matrix Af);
