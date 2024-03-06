c26b551abf6490dc8fd0fc84fe24088c669464ad
remigerme <remi.germe@gmail.com>
2024-03-06 16:36:41 +0100

    includes

--- cp.h
+++ cp.h
@@ -1,9 +1,11 @@
+#include "attribute.h"
+#include "circuit.h"
 #include "common.h"
 
 void Setup(poly_matrix B, poly_matrix T);
 
 void Enc(poly_matrix B, circuit f, bool u, poly_matrix CTf);
 
-void KeyGen(poly_matrix B, poly_matrix T, attribute x, poly_matrix SK_x);
+void KeyGen(poly_matrix B, poly_matrix T, attribute x, poly_matrix Tx);
 
-void Dec(attribute x, poly_matrix SK_x, circuit f, poly_matrix CTf, bool u);
+void Dec(attribute x, poly_matrix Tx, circuit f, poly_matrix CTf, bool u);
