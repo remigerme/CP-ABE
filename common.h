8ab3ddaf7896e9f598d2ea30ca78cff48a929150
remigerme <remi.germe@gmail.com>
2024-03-07 17:32:15 +0100

    dummy param_m (yet to determine)

--- common.h
+++ common.h
@@ -4,11 +4,12 @@
 #include <stdint.h>
 
 // Dummy parameters
-#define PARAM_N 1024        // size of polynomials
-#define PARAM_Q 1073707009  // modulus
-#define PARAM_K 30          // attribute length
-#define PARAM_SIGMA 7.00    // used for discrete gaussian distribution
-#define PARAM_TAU 80        // used for discrete gaussian distribution
+#define PARAM_N 1024           // size of polynomials
+#define PARAM_Q 1073707009     // modulus
+#define PARAM_K 30             // attribute length
+#define PARAM_M (PARAM_K + 2)  // yet to determine
+#define PARAM_SIGMA 7.00       // used for discrete gaussian distribution
+#define PARAM_TAU 80           // used for discrete gaussian distribution
 
 typedef uint32_t scalar;
 typedef int32_t signed_scalar;
