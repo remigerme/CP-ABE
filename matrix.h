0d5e53df56a8a5f2838e14f39b374cdd26711ae9
remigerme <remi.germe@gmail.com>
2024-03-22 20:28:47 +0100

    improved readibility

--- matrix.h
+++ matrix.h
@@ -2,21 +2,37 @@
 
 #include "common.h"
 
+/* -------------------- */
+/* Functions for matrix */
+/* -------------------- */
+
 // Returns a fresh heap-allocated copy of A
 matrix copy_matrix(matrix A, int d1, int d2);
-poly_matrix copy_poly_matrix(poly_matrix A, int d1, int d2);
 
 // R <- A + B
 void add_matrix(matrix A, matrix B, matrix R, int d1, int d2);
+
+// R <- A - B
+void sub_matrix(matrix A, matrix B, matrix R, int d1, int d2);
+
+// R <- A * B
+void mul_matrix(matrix A, matrix B, matrix R, int d1, int d2, int d3);
+
+/* ------------------------- */
+/* Functions for poly_matrix */
+/* ------------------------- */
+
+// Returns a fresh heap-allocated copy of A
+poly_matrix copy_poly_matrix(poly_matrix A, int d1, int d2);
+
+// R <- A + B
 void add_poly_matrix(poly_matrix A, poly_matrix B, poly_matrix R, int d1,
                      int d2);
 
 // R <- A - B
-void sub_matrix(matrix A, matrix B, matrix R, int d1, int d2);
 void sub_poly_matrix(poly_matrix A, poly_matrix B, poly_matrix R, int d1,
                      int d2);
 
 // R <- A * B
-void mul_matrix(matrix A, matrix B, matrix R, int d1, int d2, int d3);
 void mul_poly_matrix(poly_matrix A, poly_matrix B, poly_matrix R, int d1,
                      int d2, int d3);
