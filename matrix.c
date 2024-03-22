09a05d13aed1b9bf1977ac2590d8e1e774ec54ab
remigerme <remi.germe@gmail.com>
2024-03-22 20:16:25 +0100

    matrix functions

--- matrix.c
+++ matrix.c
@@ -4,6 +4,62 @@
 
 #include "poly.h"
 
+/* -------------------- */
+/* Functions for matrix */
+/* -------------------- */
+
+matrix copy_matrix(poly_matrix A, int d1, int d2) {
+    poly_matrix R = malloc(sizeof(scalar) * d1 * d2);
+    for (int i = 0; i < d1; i++) {
+        for (int j = 0; j < d2; j++) {
+            *matrix_element(R, d2, i, j) = matrix_element(A, d2, i, j);
+        }
+    }
+    return R;
+}
+
+// R <- A + B
+void add_matrix(matrix A, matrix B, matrix R, int d1, int d2) {
+    for (int i = 0; i < d1; i++) {
+        for (int j = 0; j < d2; j++) {
+            scalar a = matrix_element(A, d2, i, j);
+            scalar b = matrix_element(B, d2, i, j);
+            *matrix_element(R, d2, i, j) = (a + b) % PARAM_Q;
+        }
+    }
+}
+
+// R <- A - B
+void sub_matrix(matrix A, matrix B, matrix R, int d1, int d2) {
+    for (int i = 0; i < d1; i++) {
+        for (int j = 0; j < d2; j++) {
+            scalar a = matrix_element(A, d2, i, j);
+            scalar b = matrix_element(B, d2, i, j);
+            // Adding Q artificially to avoid underflow
+            *matrix_element(R, d2, i, j) = (PARAM_Q + a - b) % PARAM_Q;
+        }
+    }
+}
+
+// R <- A * B
+void mul_matrix(matrix A, matrix B, matrix R, int d1, int d2, int d3) {
+    for (int i = 0; i < d1; i++) {
+        for (int j = 0; j < d3; j++) {
+            scalar r = 0;
+            for (int k = 0; k < d2; k++) {
+                scalar a = matrix_element(A, d2, i, k);
+                scalar b = matrix_element(B, d3, k, j);
+                r = (r + a * b) % PARAM_Q;
+            }
+            *matrix_element(R, d3, i, j) = r;
+        }
+    }
+}
+
+/* ------------------------- */
+/* Functions for poly_matrix */
+/* ------------------------- */
+
 poly_matrix copy_poly_matrix(poly_matrix A, int d1, int d2) {
     poly_matrix R = malloc(sizeof(scalar) * PARAM_N * d1 * d2);
     for (int i = 0; i < d1; i++) {
