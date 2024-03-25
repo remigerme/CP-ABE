a0894748a2d8a343030d20515432ae74c7a98412
remigerme <remi.germe@gmail.com>
2024-03-25 14:02:59 +0100

    sampling for Zq uniform and dgs Z centered

--- sampling.c
+++ sampling.c
@@ -13,6 +13,12 @@ void sample_Rq_uniform(poly a, sampler s) {
     }
 }
 
+void sample_Zq_uniform_matrix(matrix A, int d1, int d2, sampler s) {
+    for (int i = 0; i < d1; i++)
+        for (int j = 0; j < d2; j++)
+            *matrix_element(A, d2, i, j) = uniform_mod_q(s.rng);
+}
+
 void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s) {
     for (int i = 0; i < d1; i++) {
         for (int j = 0; j < d2; j++) {
@@ -20,6 +26,14 @@ void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s) {
             sample_Rq_uniform(a, s);
         }
     }
+}
+
+signed_scalar sample_Z_centered(sampler s) { return signed_scalar_dgs(s.D); }
+
+void sample_Zq_centered_matrix(signed_matrix A, int d1, int d2, sampler s) {
+    for (int i = 0; i < d1; i++)
+        for (int j = 0; j < d2; j++)
+            *matrix_element(A, d2, i, j) = sample_Z_centered(s);
 }
 
 void sample_R_centered(signed_poly a, sampler s) {
--- sampling.h
+++ sampling.h
@@ -16,11 +16,17 @@ sampler create_sampler(void);
 // Assigns uniformely a random polynomial in Rq
 void sample_Rq_uniform(poly a, sampler s);
 
+// Assigns uniformely scalars in Zq over a random matrix of size d1 * d2
+void sample_Zq_uniform_matrix(matrix A, int d1, int d2, sampler s);
+
 // Assigns uniformely polynomials in Rq over a random matrix of size d1 * d2
 void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s);
 
 // Samples from D_{R,sigma} where R = Z[X] / <X^n+1> is isomorphic to Z^n
 void sample_R_centered(signed_poly a, sampler s);
+
+// Samples an entire matrix of size d1 * d2 from D_{Zq,sigma}
+void sample_Zq_centered_matrix(signed_matrix A, int d1, int d2, sampler s);
 
 /*
 Samples an entire matrix of size d1 * d2 from D_{R,sigma}
