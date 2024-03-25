6d765f574fc4a4e84c17cfff688a62aafafe192b
remigerme <remi.germe@gmail.com>
2024-03-25 14:15:55 +0100

    readibility (matrix and poly_matrix)

--- sampling.c
+++ sampling.c
@@ -1,5 +1,10 @@
 #include "sampling.h"
 
+typedef struct _sampler {
+    CSPRNG rng;              // for uniform distribution
+    dgs_disc_gauss_dp_t *D;  // for gaussian discrete distribution
+} sampler;
+
 sampler create_sampler() {
     CSPRNG rng = create_csprng();
     dgs_disc_gauss_dp_t *D = create_dgs();
@@ -7,16 +12,32 @@ sampler create_sampler() {
     return s;
 }
 
-void sample_Rq_uniform(poly a, sampler s) {
-    for (int i = 0; i < PARAM_N; i++) {
-        a[i] = uniform_mod_q(s.rng);
-    }
-}
+/* -------------------- */
+/* Functions for matrix */
+/* -------------------- */
 
 void sample_Zq_uniform_matrix(matrix A, int d1, int d2, sampler s) {
     for (int i = 0; i < d1; i++)
         for (int j = 0; j < d2; j++)
             *matrix_element(A, d2, i, j) = uniform_mod_q(s.rng);
+}
+
+signed_scalar sample_Z_centered(sampler s) { return signed_scalar_dgs(s.D); }
+
+void sample_Zq_centered_matrix(signed_matrix A, int d1, int d2, sampler s) {
+    for (int i = 0; i < d1; i++)
+        for (int j = 0; j < d2; j++)
+            *matrix_element(A, d2, i, j) = sample_Z_centered(s);
+}
+
+/* ------------------------- */
+/* Functions for poly_matrix */
+/* ------------------------- */
+
+void sample_Rq_uniform(poly a, sampler s) {
+    for (int i = 0; i < PARAM_N; i++) {
+        a[i] = uniform_mod_q(s.rng);
+    }
 }
 
 void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s) {
@@ -26,14 +47,6 @@ void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s) {
             sample_Rq_uniform(a, s);
         }
     }
-}
-
-signed_scalar sample_Z_centered(sampler s) { return signed_scalar_dgs(s.D); }
-
-void sample_Zq_centered_matrix(signed_matrix A, int d1, int d2, sampler s) {
-    for (int i = 0; i < d1; i++)
-        for (int j = 0; j < d2; j++)
-            *matrix_element(A, d2, i, j) = sample_Z_centered(s);
 }
 
 void sample_R_centered(signed_poly a, sampler s) {
