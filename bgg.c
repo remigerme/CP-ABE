c94af755fbc22f908e62671ac69b6b589e96d913
remigerme <remi.germe@gmail.com>
2024-03-27 18:03:13 +0100

    OfflineEnc implem (still to check)

--- bgg.c
+++ bgg.c
@@ -2,8 +2,7 @@
 
 #include "matrix.h"
 
-// Dimension à déterminer à transposition près
-int DIM_TODO = 1;
+#define G 2  // temp placeholder
 
 void BGG_KeyGen(circuit f, sampler s, matrix *A, signed_matrix Tf) {
     // Generate A1, ..., Ak uniformely over Zq^{n * l}
@@ -23,8 +22,8 @@ void BGG_KeyGen(circuit f, sampler s, matrix *A, signed_matrix Tf) {
 
 void BGG_OfflineEnc(matrix *A, bool u, sampler s, matrix *CTf) {
     // Generating LWE uniform secret
-    scalar secret[PARAM_M * PARAM_N];
-    sample_Zq_uniform_matrix(secret, PARAM_M, PARAM_N, s);
+    scalar S[PARAM_M * PARAM_N];
+    sample_Zq_uniform_matrix(S, PARAM_M, PARAM_N, s);
 
     // Short gaussian error vector
     signed_scalar E[PARAM_M * PARAM_L];
@@ -34,7 +33,22 @@ void BGG_OfflineEnc(matrix *A, bool u, sampler s, matrix *CTf) {
     if (u) {
         sample_Zq_uniform_matrix(CTf[0], PARAM_M, PARAM_L, s);
     } else {
-        mul_matrix(A[0], secret, CTf[0], PARAM_M, PARAM_N, PARAM_L);
+        mul_matrix(A[0], S, CTf[0], PARAM_M, PARAM_N, PARAM_L);
         add_matrix_error(CTf[0], E, CTf[0], PARAM_M, PARAM_L);
+    }
+
+    // Computing S * G only once
+    scalar SG[PARAM_M * PARAM_L];
+    mul_matrix(S, G, SG, PARAM_M, PARAM_N, PARAM_L);
+
+    for (int i = 0; i < PARAM_K; i++) {
+        for (int b = 0; b < 2; b++) {
+            matrix R = zero(PARAM_N, PARAM_L);
+            sample_Z_centered_matrix(E, PARAM_M, PARAM_L, s);
+            mul_matrix(S, A[1 + i], R, PARAM_M, PARAM_N, PARAM_L);
+            if (b) add_matrix(R, SG, R, PARAM_M, PARAM_L);
+            add_matrix_error(R, E, R, PARAM_M, PARAM_L);
+            CTf[1 + 2 * i + b] = R;
+        }
     }
 }
