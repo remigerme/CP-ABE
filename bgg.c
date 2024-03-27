3d5950df2b9999c58d3459dafddcd5f8d2d4b00e
remigerme <remi.germe@gmail.com>
2024-03-27 16:14:19 +0100

    updating KeyGen

--- bgg.c
+++ bgg.c
@@ -2,21 +2,25 @@
 
 #include "matrix.h"
 
-void BGG_KeyGen(circuit f, sampler s, poly_matrix A, signed_poly_matrix Tf) {
-    // Generate A1, ..., Ak uniformely over Rq^m
-    poly_matrix A_start = poly_matrix_element(A, PARAM_M, 1, 0);
-    sample_Rq_uniform_matrix(A_start, PARAM_K, PARAM_M, s);
+// Dimension à déterminer à transposition près
+int DIM_TODO = 1;
+
+void BGG_KeyGen(circuit f, sampler s, matrix *A, signed_matrix Tf) {
+    // Generate A1, ..., Ak uniformely over Zq^{n * l}
+    for (int i = 0; i < PARAM_K; i++)
+        sample_Zq_uniform_matrix(A[i + 1], PARAM_N, PARAM_L, s);
 
     // Compute Af
-    scalar Af[PARAM_N * PARAM_M];
-    compute_Af(A_start, f, Af);
+    scalar Af[PARAM_N * PARAM_L];
+    compute_Af(A + 1, f, Af);
 
     // Generate Tf
-    sample_R_centered_matrix(Tf, PARAM_M, PARAM_M, s);
+    // DIM_TODO probably = PARAM_L
+    sample_Zq_centered_matrix(Tf, DIM_TODO, DIM_TODO, s);
 
     // Compute A0
     // TODO : check types (signed)
-    mul_poly_matrix(Af, Tf, A, 1, PARAM_M, PARAM_M);
+    mul_matrix(Af, Tf, A, 1, DIM_TODO, DIM_TODO);
 }
 
 void BGG_OfflineEnc(poly_matrix A, bool u, sampler s, poly_matrix CTf) {
--- bgg.h
+++ bgg.h
@@ -4,13 +4,10 @@
 
 /*
 Given a circuit f (and a sampler s) computes
-      | A0 |
-  A = | A1 | in Rq^{(k + 1) * m}
-      | .. |
-      | Ak |
-  Tf in R^{m*m} the trap used to compute A0
+  - A = [A0, A1, ..., Ak] where Ai in Zq^{n * l}
+  - Tf in Z^{l * l} the trap used to compute A0
 */
-void BGG_KeyGen(circuit f, sampler s, poly_matrix A, signed_poly_matrix Tf);
+void BGG_KeyGen(circuit f, sampler s, matrix *A, signed_matrix Tf);
 
 /*
 Given a bit to encrypt u, a sampler s and A
