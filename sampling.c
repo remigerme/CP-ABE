7f27ef6080b88ce5c9d540dea3150ab92b6ef491
remigerme <remi.germe@gmail.com>
2024-03-31 22:11:06 +0200

    sampling using new matrix struct (and no more poly)

--- sampling.c
+++ sampling.c
@@ -11,50 +11,16 @@ sampler create_sampler() {
 /* Functions for matrix */
 /* -------------------- */
 
-void sample_Zq_uniform_matrix(matrix A, int d1, int d2, sampler s) {
-    for (int i = 0; i < d1; i++)
-        for (int j = 0; j < d2; j++)
-            matrix_element(A, d2, i, j) = uniform_mod_q(s.rng);
+void sample_Zq_uniform_matrix(matrix A, sampler s) {
+    for (int i = 0; i < A->rows; i++)
+        for (int j = 0; j < A->columns; j++)
+            matrix_element(A, i, j) = uniform_mod_q(s.rng);
 }
 
 signed_scalar sample_Z_centered(sampler s) { return signed_scalar_dgs(s.D); }
 
-void sample_Z_centered_matrix(signed_matrix A, int d1, int d2, sampler s) {
-    for (int i = 0; i < d1; i++)
-        for (int j = 0; j < d2; j++)
-            matrix_element(A, d2, i, j) = sample_Z_centered(s);
-}
-
-/* ------------------------- */
-/* Functions for poly_matrix */
-/* ------------------------- */
-
-void sample_Rq_uniform(poly a, sampler s) {
-    for (int i = 0; i < PARAM_N; i++) {
-        a[i] = uniform_mod_q(s.rng);
-    }
-}
-
-void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s) {
-    for (int i = 0; i < d1; i++) {
-        for (int j = 0; j < d2; j++) {
-            poly a = poly_matrix_element(A, d2, i, j);
-            sample_Rq_uniform(a, s);
-        }
-    }
-}
-
-void sample_R_centered(signed_poly a, sampler s) {
-    for (int i = 0; i < PARAM_N; i++) {
-        a[i] = signed_scalar_dgs(s.D);
-    }
-}
-
-void sample_R_centered_matrix(signed_poly_matrix A, int d1, int d2, sampler s) {
-    for (int i = 0; i < d1; i++) {
-        for (int j = 0; j < d2; j++) {
-            signed_poly a = poly_matrix_element(A, d2, i, j);
-            sample_R_centered(a, s);
-        }
-    }
+void sample_Z_centered_matrix(signed_matrix A, sampler s) {
+    for (int i = 0; i < A->rows; i++)
+        for (int j = 0; j < A->columns; j++)
+            matrix_element(A, i, j) = sample_Z_centered(s);
 }
--- sampling.h
+++ sampling.h
@@ -1,4 +1,6 @@
-#include "common.h"
+#pragma once
+
+#include "matrix.h"
 #include "random.h"
 
 /*
@@ -12,28 +14,15 @@ typedef struct _sampler {
     dgs_disc_gauss_dp_t *D;  // for gaussian discrete distribution
 } sampler;
 
-// Create a sampler, used by sampling functions to generate random results
-sampler create_sampler(void);
-
 /* -------------------- */
 /* Functions for matrix */
 /* -------------------- */
 
-// Assigns uniformely scalars in Zq over a random matrix of size d1 * d2
-void sample_Zq_uniform_matrix(matrix A, int d1, int d2, sampler s);
+// Create a sampler, used by sampling functions to generate random results
+sampler create_sampler(void);
 
-// Samples an entire matrix of size d1 * d2 from D_{Zq,sigma}
-void sample_Z_centered_matrix(signed_matrix A, int d1, int d2, sampler s);
+// Assigns uniformely scalars in Zq over a random matrix
+void sample_Zq_uniform_matrix(matrix A, sampler s);
 
-/* ------------------------- */
-/* Functions for poly_matrix */
-/* ------------------------- */
-
-// Assigns uniformely polynomials in Rq over a random matrix of size d1 * d2
-void sample_Rq_uniform_matrix(poly_matrix A, int d1, int d2, sampler s);
-
-/*
-Samples an entire matrix of size d1 * d2 from D_{R,sigma}
-where R = Z[X] / <X^n+1> is isomorphic to Z^n
-*/
-void sample_R_centered_matrix(signed_poly_matrix A, int d1, int d2, sampler s);
+// Samples an entire matrix from D_{Zq,sigma}
+void sample_Z_centered_matrix(signed_matrix A, sampler s);
