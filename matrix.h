27a528e943a9f0f71693791423e210f523170baa
remigerme <remi.germe@gmail.com>
2024-03-31 20:15:38 +0200

    matrix structs and functions signatures

--- matrix.h
+++ matrix.h
@@ -6,26 +6,41 @@
 /* Functions for matrix */
 /* -------------------- */
 
+typedef struct matrix {
+    unsigned int rows;
+    unsigned int columns;
+    scalar* data;
+} matrix;
+
+typedef struct signed_matrix {
+    unsigned int rows;
+    unsigned int columns;
+    signed_scalar* data;
+} signed_matrix;
+
+// Accessing (signed ?) scalar Mi,j
+#define matrix_element(M, i, j) (M[((i) * (M.columns)) + (j)])
+
 // Returns a heap-allocated matrix filled with 0s
-matrix zeros(int d1, int d2);
+matrix zeros_matrix(int rows, int columns);
 
 // Returns a fresh heap-allocated copy of A
-matrix copy_matrix(matrix A, int d1, int d2);
+matrix copy_matrix(matrix A);
 
 // R <- A + B
-void add_matrix(matrix A, matrix B, matrix R, int d1, int d2);
+void add_matrix(matrix A, matrix B, matrix R);
 
 // R <- A + E
-void add_matrix_error(matrix A, signed_matrix E, matrix R, int d1, int d2);
+void add_matrix_error(matrix A, signed_matrix E, matrix R);
 
 // R <- A - B
-void sub_matrix(matrix A, matrix B, matrix R, int d1, int d2);
+void sub_matrix(matrix A, matrix B, matrix R);
 
 // R <- A * B
-void mul_matrix(matrix A, matrix B, matrix R, int d1, int d2, int d3);
+void mul_matrix(matrix A, matrix B, matrix R);
 
 // R <- A * Tf where A in Zq^{d1 * d2} and Tf in Z^{d2 * d2}
-void mul_matrix_trap(matrix A, signed_matrix Tf, matrix R, int d1, int d2);
+void mul_matrix_trap(matrix A, signed_matrix Tf, matrix R);
 
 /* ------------------------- */
 /* Functions for poly_matrix */
