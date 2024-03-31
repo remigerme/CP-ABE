a48c37418a21fe4c8e3e19bcfe7fe6cb974e3a5a
remigerme <remi.germe@gmail.com>
2024-03-31 23:09:42 +0200

    BGG new matrix struct (will bug i think)

--- bgg.c
+++ bgg.c
@@ -6,49 +6,52 @@
 
 void BGG_KeyGen(circuit f, sampler s, matrix *A, signed_matrix Tf) {
     // Generate A1, ..., Ak uniformely over Zq^{n * l}
-    for (int i = 0; i < PARAM_K; i++)
-        sample_Zq_uniform_matrix(A[i + 1], PARAM_N, PARAM_L, s);
+    for (int i = 0; i < PARAM_K; i++) sample_Zq_uniform_matrix(A[i + 1], s);
 
     // Compute Af
-    scalar Af[PARAM_N * PARAM_L];
-    compute_Af(A + 1, f, Af);
+    matrix Af = new_matrix(PARAM_N, PARAM_L);
+    compute_Af(A, f, Af);
 
     // Generate Tf
-    sample_Z_centered_matrix(Tf, PARAM_L, PARAM_L, s);
+    sample_Z_centered_matrix(Tf, s);
 
     // Compute A0
-    mul_matrix_trap(Af, Tf, A[0], PARAM_N, PARAM_L);
+    mul_matrix_trap(Af, Tf, A[0]);
 }
 
 void BGG_OfflineEnc(matrix *A, bool u, sampler s, matrix *CTf) {
     // Generating LWE uniform secret
-    scalar S[PARAM_M * PARAM_N];
-    sample_Zq_uniform_matrix(S, PARAM_M, PARAM_N, s);
+    matrix S = new_matrix(PARAM_M, PARAM_N);
+    sample_Zq_uniform_matrix(S, s);
 
     // Short gaussian error vector
-    signed_scalar E[PARAM_M * PARAM_L];
-    sample_Z_centered_matrix(E, PARAM_M, PARAM_L, s);
+    signed_matrix E = new_signed_matrix(PARAM_M, PARAM_L);
+    sample_Z_centered_matrix(E, s);
 
     // Instantiating first term of CTf (`u` in p.12 or C0 in p.13)
+    matrix CTf_0 = new_matrix(PARAM_M, PARAM_L);
     if (u) {
-        sample_Zq_uniform_matrix(CTf[0], PARAM_M, PARAM_L, s);
+        sample_Zq_uniform_matrix(CTf_0, s);
     } else {
-        mul_matrix(A[0], S, CTf[0], PARAM_M, PARAM_N, PARAM_L);
-        add_matrix_error(CTf[0], E, CTf[0], PARAM_M, PARAM_L);
+        mul_matrix(A[0], S, CTf_0);
+        add_matrix_error(CTf_0, E, CTf_0);
     }
+    CTf[0] = CTf_0;
 
     // Computing S * G only once
-    scalar SG[PARAM_M * PARAM_L];
-    mul_matrix(S, G, SG, PARAM_M, PARAM_N, PARAM_L);
+    matrix SG = new_matrix(PARAM_M, PARAM_L);
+    mul_matrix(S, G, SG);
 
     for (int i = 0; i < PARAM_K; i++) {
         for (int b = 0; b < 2; b++) {
-            matrix R = zeros(PARAM_N, PARAM_L);
-            sample_Z_centered_matrix(E, PARAM_M, PARAM_L, s);
-            mul_matrix(S, A[1 + i], R, PARAM_M, PARAM_N, PARAM_L);
-            if (b) add_matrix(R, SG, R, PARAM_M, PARAM_L);
-            add_matrix_error(R, E, R, PARAM_M, PARAM_L);
+            matrix R = new_matrix(PARAM_N, PARAM_L);
+            sample_Z_centered_matrix(E, s);
+            mul_matrix(S, A[1 + i], R);
+            if (b) add_matrix(R, SG, R);
+            add_matrix_error(R, E, R);
             CTf[1 + 2 * i + b] = R;
         }
     }
+
+    free_matrix(SG);
 }
