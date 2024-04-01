7d3b0dcf6216b0c2ebd5f1ca89f324893439931b
remigerme <remi.germe@gmail.com>
2024-04-01 12:31:33 +0200

    new convention for pointers

--- circuit.h
+++ circuit.h
@@ -11,8 +11,8 @@ at a leaf ie left == right == NULL, in which case
 it stores the relevant index `n` (n = 1 to consider A1).
 */
 typedef struct btree {
-    struct btree *left;
-    struct btree *right;
+    struct btree* left;
+    struct btree* right;
     int n;
 } circuit;
 
@@ -21,11 +21,11 @@ Given A = [A0, A1, ..., Ak] (A0 is yet to compute)
 where Ai in Zq^{n * l}, and a boolean circuit f
 composed of NAND gates, we compute Af = f(A1, ..., Ak).
 */
-void compute_Af(matrix *A, circuit f, matrix Af);
+void compute_Af(matrix* A, circuit f, matrix Af);
 
 /*
 Given A = [A0, A1, ..., Ak] where Ai in Zq^{n * l},
 a boolean circuit f and an attribute x, we compute
 the associated Hf,x,A matrix.
 */
-void compute_H(matrix *A, circuit f, attribute x, matrix H);
+void compute_H(matrix* A, circuit f, attribute x, matrix H);
