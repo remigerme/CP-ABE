0dbde65423df0438f4dab4faf1f3ab587dcd51e0
remigerme <remi.germe@gmail.com>
2024-04-03 01:29:15 +0200

    no longer in place for CTf

--- bgg.c
+++ bgg.c
@@ -2,22 +2,24 @@
 
 #include "common.h"
 
-void BGG_KeyGen(circuit f, sampler s, matrix *A, signed_matrix Tf) {
+void BGG_KeyGen(circuit f, sampler s, matrix* A, signed_matrix Tf) {
     // Generate A1, ..., Ak uniformely over Zq^{n * l}
     for (int i = 0; i < PARAM_K; i++) sample_Zq_uniform_matrix(A[i + 1], s);
 
     // Compute Af
-    matrix Af = new_matrix(PARAM_N, PARAM_L);
-    compute_Af(A, f, Af);
+    matrix Af = compute_Af(A, f);
 
     // Generate Tf
     sample_Z_centered_matrix(Tf, s);
 
     // Compute A0
     mul_matrix_trap(Af, Tf, A[0]);
+    free_matrix(Af);
 }
 
-void BGG_OfflineEnc(matrix *A, bool u, sampler s, matrix *CTf) {
+matrix* BGG_OfflineEnc(matrix* A, bool u, sampler s) {
+    matrix* CTf = new_matrixes(2 * PARAM_K + 1, PARAM_M, PARAM_L);
+
     // Generating LWE uniform secret
     matrix S = new_matrix(PARAM_M, PARAM_N);
     sample_Zq_uniform_matrix(S, s);
@@ -27,14 +29,12 @@ void BGG_OfflineEnc(matrix *A, bool u, sampler s, matrix *CTf) {
     sample_Z_centered_matrix(E, s);
 
     // Instantiating first term of CTf (`u` in p.12 or C0 in p.13)
-    matrix CTf_0 = new_matrix(PARAM_M, PARAM_L);
-    if (u) {
-        sample_Zq_uniform_matrix(CTf_0, s);
-    } else {
-        mul_matrix(A[0], S, CTf_0);
-        add_matrix_error(CTf_0, E, CTf_0);
+    if (u)
+        sample_Zq_uniform_matrix(CTf[0], s);
+    else {
+        mul_matrix(A[0], S, CTf[0]);
+        add_matrix_error(CTf[0], E, CTf[0]);
     }
-    CTf[0] = CTf_0;
 
     // Computing S * G only once
     matrix SG = new_matrix(PARAM_M, PARAM_L);
@@ -42,14 +42,16 @@ void BGG_OfflineEnc(matrix *A, bool u, sampler s, matrix *CTf) {
 
     for (int i = 0; i < PARAM_K; i++) {
         for (int b = 0; b < 2; b++) {
-            matrix R = new_matrix(PARAM_N, PARAM_L);
             sample_Z_centered_matrix(E, s);
-            mul_matrix(S, A[1 + i], R);
-            if (b) add_matrix(R, SG, R);
-            add_matrix_error(R, E, R);
-            CTf[1 + 2 * i + b] = R;
+            mul_matrix(S, A[1 + i], CTf[1 + 2 * i + b]);
+            if (b) add_matrix(CTf[1 + 2 * i + b], SG, CTf[1 + 2 * i + b]);
+            add_matrix_error(CTf[1 + 2 * i + b], E, CTf[1 + 2 * i + b]);
         }
     }
 
+    free_matrix(S);
+    free_signed_matrix(E);
     free_matrix(SG);
+
+    return CTf;
 }
