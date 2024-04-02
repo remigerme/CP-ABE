26aada8147e3ddd9edab3912ecd7b6dfea767a3e
remigerme <remi.germe@gmail.com>
2024-04-02 22:47:40 +0200

    test circuit working !!!! HURRAH

--- test_circuit.c
+++ test_circuit.c
@@ -30,28 +30,24 @@ int main() {
 
     attribute x = 7;
     matrix H = compute_H(A, f, x);
-    print_matrix(H);
-    printf("\n");
 
     matrix R = new_matrix(PARAM_N, PARAM_L);
     add_matrix(Af, G, R);
-    print_matrix(R);
-    printf("\n");
 
     matrix T = new_matrix(PARAM_N, PARAM_L);
     matrix BIG = new_matrix(PARAM_N, PARAM_L * PARAM_K);
     for (int i = 1; i < PARAM_K + 1; i++) {
         matrix ti = copy_matrix(A[i]);
         add_matrix(ti, G, ti);
-        for (int j = 0; j < ti->rows; j++)
-            for (int k = 0; k < ti->columns; k++)
+        for (int j = 0; j < ti->rows; j++)         // ti->rows = PARAM_N
+            for (int k = 0; k < ti->columns; k++)  // ti->columns = PARAM_L
                 matrix_element(BIG, j, (i - 1) * PARAM_L + k) =
                     matrix_element(ti, j, k);
         free_matrix(ti);
     }
     mul_matrix(BIG, H, T);
-    print_matrix(T);
-    printf("\n");
+
+    assert(equals(R, T));
 
     free_matrix(R);
     free_matrix(BIG);
