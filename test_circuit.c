ee7264fc33d4e8031987d88e35a5be708bea96b5
remigerme <remi.germe@gmail.com>
2024-04-05 01:00:28 +0200

    printing parameters and benchmarking each step using CHRONO

--- test_circuit.c
+++ test_circuit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "attribute.h"
 #include "circuit.h"
@@ -7,22 +8,35 @@
 
 int main() {
     sampler s = create_sampler();
-
     init_G();
+    real start, end;
 
+    // Printing parameters
+    printf("Testing circuit with parameters\n");
+    printf("\tN = %d\n", PARAM_N);
+    printf("\tK = %d\n", PARAM_K);
+    printf("\tL = %d\n", PARAM_L);
+    printf("\tA matrixes are size : N * L = %d\n", PARAM_N * PARAM_L);
+    printf("\tT matrixes are size : L * L = %d\n", PARAM_L * PARAM_L);
+    printf("\tH matrixes are size : K * L * L = %d\n",
+           PARAM_K * PARAM_L * PARAM_L);
+
+    // Generating A
     matrix* A = new_matrixes(PARAM_K + 1, PARAM_N, PARAM_L);
-    for (int i = 0; i < PARAM_K + 1; i++) sample_Zq_uniform_matrix(A[i], s);
+    CHRONO("Generated A in %fs\n", {
+        for (int i = 0; i < PARAM_K + 1; i++) sample_Zq_uniform_matrix(A[i], s);
+    });
 
     // Testing G * G^-1(A) = A
-    printf("G * G^-1(A) = A : ");
     matrix inv = new_matrix(PARAM_L, PARAM_L);
     matrix res = new_matrix(PARAM_N, PARAM_L);
-    inv_G(A[0], inv);
-    mul_matrix(G, inv, res);
-    assert(equals(A[0], res));
+    CHRONO("Checked G * G^-1(A) = A in %fs\n", {
+        inv_G(A[0], inv);
+        mul_matrix(G, inv, res);
+        assert(equals(A[0], res));
+    });
     free_matrix(inv);
     free_matrix(res);
-    printf("done\n");
 
     circuit f;
     circuit g;
@@ -41,29 +55,31 @@ int main() {
 
     int x_max = 1;
     for (int i = 0; i < PARAM_K; i++) x_max *= 2;
+    char output[80];
 
     for (attribute x = 0; x < x_max; x++) {
-        printf("BIG * H = Af + f(x)G for x = %d : ", x);
-        matrix H = compute_H(A, f, x);
-        matrix R = copy_matrix(Af);
-        if (compute_f(f, x)) add_matrix(R, G, R);
+        sprintf(output, "BIG * H = Af + f(x)G for x = %d : done in %%fs\n", x);
+        CHRONO(output, {
+            matrix H = compute_H(A, f, x);
+            matrix R = copy_matrix(Af);
+            if (compute_f(f, x)) add_matrix(R, G, R);
 
-        for (int i = 1; i < PARAM_K + 1; i++) {
-            matrix ti = copy_matrix(A[i]);
-            if (get_xn(x, i)) add_matrix(ti, G, ti);
-            for (int j = 0; j < ti->rows; j++)         // ti->rows = PARAM_N
-                for (int k = 0; k < ti->columns; k++)  // ti->columns = PARAM_L
-                    matrix_element(BIG, j, (i - 1) * PARAM_L + k) =
-                        matrix_element(ti, j, k);
-            free_matrix(ti);
-        }
-        mul_matrix(BIG, H, T);
+            for (int i = 1; i < PARAM_K + 1; i++) {
+                matrix ti = copy_matrix(A[i]);
+                if (get_xn(x, i)) add_matrix(ti, G, ti);
+                for (int j = 0; j < ti->rows; j++)  // ti->rows = PARAM_N
+                    for (int k = 0; k < ti->columns;
+                         k++)  // ti->columns = PARAM_L
+                        matrix_element(BIG, j, (i - 1) * PARAM_L + k) =
+                            matrix_element(ti, j, k);
+                free_matrix(ti);
+            }
+            mul_matrix(BIG, H, T);
 
-        assert(equals(R, T));
-        printf("done\n");
-
-        free_matrix(H);
-        free_matrix(R);
+            assert(equals(R, T));
+            free_matrix(H);
+            free_matrix(R);
+        });
     }
 
     free_matrix(BIG);
