ecbe10d5337098ccbce4dde43ff7ab305a9e5cc0
remigerme <remi.germe@gmail.com>
2024-04-05 14:49:21 +0200

    correct header for dec

--- cp.h
+++ cp.h
@@ -20,4 +20,4 @@ cp_ciphertext Enc(matrix* B, circuit f, bool u);
 
 void KeyGen(matrix* B, matrix T, attribute x, matrix Tx);
 
-bool Dec(attribute x, matrix Tx, circuit f, matrix* CTf);
+bool Dec(attribute x, circuit f, bgg_keys keys, signed_matrix tx, matrix* CTf);
