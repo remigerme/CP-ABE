01e0b50320483eb2febfe942d204703d31eb4e40
remigerme <remi.germe@gmail.com>
2024-04-10 19:44:51 +0200

    full test cp with manually set circuit and attribute

--- src/test/test_cp.c
+++ src/test/test_cp.c
@@ -59,11 +59,50 @@ int main() {
     CHRONO("Computed ciphertext for u = 1 in %fs\n",
            res_1 = Enc(keys.B, f, 1););
 
-    free_signed_matrix(res_0.Tf);
+    attribute x = 3;
+    assert(compute_f(f, x) == 0);
+    printf("Attribute x is authorized to decrypt.\n");
+
+    signed_matrix Tx;
+    CHRONO("Generating Tx trap in %fs\n", Tx = KeyGen(keys.B, keys.T, x););
+    mul_matrix_trap_left(Tx, keys.B[0], res);
+    assert(equals(res, ZERO));
+    for (int i = 0; i < PARAM_K; i++) {
+        bool xi = get_xn(x, i + 1);  // 1 indexed
+        // Checking TxBi,xi = 0
+        mul_matrix_trap_left(Tx, keys.B[1 + 2 * i + xi], res);
+        assert(equals(res, ZERO));
+        // Checking TxBi,1-xi != 0, should check if uniformely distributed
+        mul_matrix_trap_left(Tx, keys.B[1 + 2 * i + (1 - xi)], res);
+        assert(!equals(res, ZERO));
+    }
+    printf("Done checking TxBi,b relations\n");
+
+    bool plain_0, plain_1;
+    CHRONO("Deciphering (for plain = 0) in %fs\n", {
+        plain_0 = Dec(x, f, keys.T, res_0);
+        assert(plain_0 == 0);
+    });
+    CHRONO("Deciphering (for plain = 1) in %fs\n", {
+        plain_1 = Dec(x, f, keys.T, res_1);
+        assert(plain_1 == 1);
+    });
+
+    // Free time !
     free_matrixes(res_0.CTf, 2 * PARAM_K + 1);
-    free_signed_matrix(res_1.Tf);
+    free_matrixes(res_0.A, PARAM_K);
+    free_signed_matrix(res_0.Tf);
+
     free_matrixes(res_1.CTf, 2 * PARAM_K + 1);
+    free_matrixes(res_1.A, PARAM_K);
+    free_signed_matrix(res_1.Tf);
+
     free_matrixes(keys.B, 2 * PARAM_K + 1);
     free_signed_matrix(keys.T);
+    free_signed_matrix(Tx);
+
+    free_matrix(ZERO);
+    free_matrix(res);
+
     free_matrix(G);
 }
