fcd848a86c55eca4220d1c88391a65a1eb35ea7b
remigerme <remi.germe@gmail.com>
2024-04-19 16:35:56 +0200

    is short c test

--- tests/test_is_short.c
+++ tests/test_is_short.c
@@ -1,5 +1,7 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "attribute.h"
 #include "bgg.h"
@@ -10,12 +12,33 @@
 #include "matrix.h"
 #include "sampling.h"
 
-int main() {
-    real start, end;
-    init_cp_default();
+int main(int argc, char* argv[]) {
+    if (argc != 8) {
+        printf(
+            "Illegal number of arguments. Usage : ./test N Q K P SIGMA "
+            "SHORT_THRESHOLD x\n");
+        return -1;
+    }
 
-    attribute x = 39837839;
-    circuit *f = gen_circuit(x);
+    int32_t N = atoi(argv[1]);
+    uint32_t Q = strtoul(argv[2], NULL, 10);
+    int32_t K = atoi(argv[3]);
+    int32_t P = atoi(argv[4]);
+    real SIGMA = strtod(argv[5], NULL);
+    real SHORT_THRESHOLD = strtod(argv[6], NULL);
+    attribute x = strtoul(argv[7], NULL, 10);
+
+    init_cp(N, Q, K, P, SIGMA, SHORT_THRESHOLD);
+
+    /*
+    Example hand-crafted small circuit f(x) = x1 & (x2 | x3)
+    Could be like checking if user is an admin
+    or is a dev and has the right to access feature
+    must have K >= 3
+    */
+    assert(K >= 3);
+    circuit* f = circuit_or(gen_leaf(1, true),
+                            circuit_and(gen_leaf(2, true), gen_leaf(3, true)));
 
     cp_keys keys = Setup();
 
@@ -24,8 +47,8 @@ int main() {
 
     signed_matrix Tx = KeyGen(keys.B, keys.T, x);
 
-    bool plain_0 = Dec(x, *f, keys.T, res_0);
-    bool plain_1 = Dec(x, *f, keys.T, res_1);
+    Dec(x, *f, keys.T, res_0);
+    Dec(x, *f, keys.T, res_1);
 
     // Free time !
     free_matrixes(res_0.CTf, 2 * PARAMS.K + 1);
